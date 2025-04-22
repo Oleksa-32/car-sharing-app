377aedfc476f49c2bac15f49be3c4f8631285550
user <graban2253@gmail.com>
2025-04-22 19:18:21 +0300

    refactored imports

--- src/main/java/com/example/carsharingapp/config/StripeConfig.java
+++ src/main/java/com/example/carsharingapp/config/StripeConfig.java
@@ -3,8 +3,6 @@ package com.example.carsharingapp.config;
 import com.stripe.Stripe;
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
-import lombok.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
