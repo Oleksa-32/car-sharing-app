9acdf9db7718587c67884e3221f2a260f47758f4
user <graban2253@gmail.com>
2025-04-22 19:40:16 +0300

    refactored STRIPE_SECRET_KEY

--- src/main/java/com/example/carsharingapp/config/StripeConfig.java
+++ src/main/java/com/example/carsharingapp/config/StripeConfig.java
@@ -1,15 +1,18 @@
 package com.example.carsharingapp.config;
 
 import com.stripe.Stripe;
-import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StripeConfig {
+
+    @Value("${STRIPE_SECRET_KEY}")
+    private String stripeSecretKey;
+
     @PostConstruct
     public void init() {
-        Dotenv dotenv = Dotenv.load();
-        Stripe.apiKey = dotenv.get("STRIPE_SECRET_KEY");
+        Stripe.apiKey = stripeSecretKey;
     }
 }
--- src/test/java/com/example/carsharingapp/CarSharingAppApplicationTests.java
+++ src/test/java/com/example/carsharingapp/CarSharingAppApplicationTests.java
@@ -3,9 +3,7 @@ package com.example.carsharingapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        "STRIPE_SECRET_KEY=${STRIPE_SECRET_KEY}"
-})
+@SpringBootTest
 class CarSharingAppApplicationTests {
 
     @Test
