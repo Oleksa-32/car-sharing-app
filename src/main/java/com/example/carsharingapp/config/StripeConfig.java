a016988f411ad4f72ff1fe3ab19edfd1bb2fca78
user <graban2253@gmail.com>
2025-04-22 19:16:48 +0300

    refactored ci

--- src/main/java/com/example/carsharingapp/config/StripeConfig.java
+++ src/main/java/com/example/carsharingapp/config/StripeConfig.java
@@ -3,6 +3,8 @@ package com.example.carsharingapp.config;
 import com.stripe.Stripe;
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
--- src/test/java/com/example/carsharingapp/CarSharingAppApplicationTests.java
+++ src/test/java/com/example/carsharingapp/CarSharingAppApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.carsharingapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "STRIPE_SECRET_KEY=${STRIPE_SECRET_KEY}"
+})
 class CarSharingAppApplicationTests {
 
     @Test
